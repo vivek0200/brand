dd90cd70ea038e49e0262754c690443a994d9a53
vivek0200 <vivekkumarvk0217@gmail.com>
2023-11-28 20:20:42 +0530

    Commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 const Navigation = () => {
 
     return(
-        <nav>
+        <nav className="container">
         <div className="logo">
           <img src="/images/brand_logo.png" alt="logo" />
         </div>
